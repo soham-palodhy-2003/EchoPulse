6fe176447850f4d9ad5f9a922fd8dcf44df7a00e
sohampalodhy8068@gmail.com <sohampalodhy8068@gmail.com>
2024-05-24 18:25:26 +0530

    card creation and API fetch

---
+++ src/components/NewsItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const NewsItem = (props) => {
+  const { title, description, imgUrl } = props
+
+  return (
+    <div className='my-3'>
+      <div className="card" style={{ width: "20rem" }}>
+        {imgUrl && (
+          <img src={imgUrl} className="card-img-top" alt={title || 'News Item'} />
+        )}
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <a href="/" className="btn btn-primary btn-sm">Read More</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewsItem;
