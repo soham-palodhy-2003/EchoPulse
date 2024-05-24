fdbd73cb9cf18a5a62ba893448defc041bd3ea31
sohampalodhy8068@gmail.com <sohampalodhy8068@gmail.com>
2024-05-24 19:24:35 +0530

    Fetching API

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
 const NewsItem = (props) => {
-  const { title, description, imgUrl } = props
+  const { title, description, imgUrl, newsUrl} = props
 
   return (
     <div className='my-3'>
-      <div className="card" style={{ width: "20rem" }}>
+      <div className="card" style={{ width: "20rem" ,}}>
         {imgUrl && (
           <img src={imgUrl} className="card-img-top" alt={title || 'News Item'} />
         )}
         <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">{description}</p>
-          <a href="/" className="btn btn-primary btn-sm">Read More</a>
+          <h5 className="card-title">{title}...</h5>
+          <p className="card-text">{description}...</p>
+          <a href={newsUrl} target='_blank' className="btn btn-primary btn-sm">Read More</a>
         </div>
       </div>
     </div>
